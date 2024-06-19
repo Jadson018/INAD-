0a1d77e4c5ca44c63ff579c0885377ef3c1e6c4e
Jadson Pamplona <jadsonpamplonaviana@gmail.com>
2024-06-19 11:55:24 -0300

    Adicionando logo ao executável

--- setup.py
+++ setup.py
@@ -15,11 +15,13 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+    
+
 # Configuração do setup
 setup(name='INAD+',
       version='2.0',
       description='Analisador de planilhas especificas',
       options={'build_exe': build_exe_options},
-      executables=[Executable("INAD+.py", base=base)]
+      executables=[Executable("INAD+.py", base=base, icon="polo_ico.ico")]
       )
 
